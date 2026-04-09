bc0a6ac1ecde5644d2e5dfaa3a0ddc335e9db5ab
Toby <tobbe@tripitaka.se>
2026-04-09 23:51:32 +1000

    Add new papers

--- update.sh
+++ update.sh
@@ -11,5 +11,9 @@ echo "✓ PDF built"
 
 cd ..
 
+mv publications/publication_list.pdf .
+
+echo "✓ PDF moved to $(pwd)"
+
 git add .
 git diff --cached --quiet && echo "Nothing to commit" || (git commit -m "Add new papers" && git push origin main && echo "✓ Pushed")
