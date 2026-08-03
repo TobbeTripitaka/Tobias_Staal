5441be97dd844155bdce0d92c056565acf3690bf
Toby <tobbe@tripitaka.se>
2026-08-03 23:56:50 +1000

    fix script

--- update.sh
+++ update.sh
@@ -9,13 +9,13 @@ biber --quiet publication_list
 pdflatex -interaction=batchmode publication_list.tex
 pdflatex -interaction=batchmode publication_list.tex
 
-echo "✓ PDF built"
+echo "PDF built"
 
 cd ..
 
 mv publications/publication_list.pdf .
 
-echo "✓ PDF moved to $(pwd)"
+echo "PDF moved to $(pwd)"
 
 git add .
 git diff --cached --quiet && echo "Nothing to commit" || (git commit -m "$MESSAGE" && git push origin main && echo "✓ Pushed")
